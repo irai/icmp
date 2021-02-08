0d2774c15f0330150d9990dacdce3ca01d082926
FB <irai852@gmail.com>
2021-02-09 02:46:39 +1100

    fix sendICMP6 packet

--- icmp6/echo6.go
+++ icmp6/echo6.go
@@ -91,7 +91,7 @@ func (h *Handler) Ping(src net.IP, dst net.IP, timeout time.Duration) (err error
 func sendICMP6Packet(ifi *net.Interface, src net.IP, dst net.IP, p []byte) error {
 
 	// TODO: reuse h.conn and write directly to socket
-	c, err := icmp.ListenPacket("ip6:ipv6-icmp", "::")
+	c, err := net.ListenPacket("ip6:ipv6-icmp", "::")
 	// c, err := net.DialIP("ip6:1", nil, nil) // ICMP for IPv6
 	if err != nil {
 		log.Error("icmp error in listen packet: ", err)
